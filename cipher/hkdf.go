86aecf99e4a15a322b645275f5486343e345e7d2
Deb Kalyan Mohanty <96584862+debkalyanmohanty@users.noreply.github.com>
2026-03-10 11:04:02 +0530

    Update hkdf.go

--- cipher/hkdf.go
+++ cipher/hkdf.go
@@ -3,21 +3,28 @@ package cipher
 import (
 	"crypto/sha256"
 	"io"
+	"strconv"
 
 	"golang.org/x/crypto/hkdf"
 )
 
 func deriveKey(secret string, timeSlot int64) ([]byte, error) {
 
+	salt := []byte(strconv.FormatInt(timeSlot, 10))
+	info := []byte("time-based-encryption")
+
 	h := hkdf.New(
 		sha256.New,
 		[]byte(secret),
-		[]byte(string(rune(timeSlot))),
-		[]byte("time-based-encryption"),
+		salt,
+		info,
 	)
 
 	key := make([]byte, 32)
-	_, err := io.ReadFull(h, key)
 
-	return key, err
+	if _, err := io.ReadFull(h, key); err != nil {
+		return nil, err
+	}
+
+	return key, nil
 }
